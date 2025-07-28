b8149ae22dcf9ba7c8b6e8b4c6740a41268b6592
Deepak Bagada <deeepakbagada25@gmail.com>
2025-07-28 21:23:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,7 +4,6 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { Skeleton } from '@/components/ui/skeleton';
-import { Header } from '@/components/layout/header';
 
 export default function AdminLayout({
   children,
@@ -27,29 +26,23 @@ export default function AdminLayout({
 
   if (isChecking) {
     return (
-        <>
-            <Header />
-            <div className="flex h-[calc(100vh-5rem)] w-full items-center justify-center">
-                <div className="space-y-4 w-full max-w-2xl p-8">
-                    <Skeleton className="h-12 w-1/2" />
-                    <Skeleton className="h-8 w-1/4" />
-                    <div className="space-y-2 pt-8">
-                        <Skeleton className="h-10 w-full" />
-                        <Skeleton className="h-32 w-full" />
-                        <Skeleton className="h-10 w-full" />
-                    </div>
-                </div>
-            </div>
-        </>
+      <div className="flex h-screen w-full items-center justify-center">
+          <div className="space-y-4 w-full max-w-2xl p-8">
+              <Skeleton className="h-12 w-1/2" />
+              <Skeleton className="h-8 w-1/4" />
+              <div className="space-y-2 pt-8">
+                  <Skeleton className="h-10 w-full" />
+                  <Skeleton className="h-32 w-full" />
+                  <Skeleton className="h-10 w-full" />
+              </div>
+          </div>
+      </div>
     );
   }
 
   return (
-    <>
-        <Header />
-        <div className="flex flex-1">
-            {children}
-        </div>
-    </>
-    );
+    <div className="flex flex-1">
+      {children}
+    </div>
+  );
 }
